e0d5ec9fe161c1669e4797bc27b4d5fe2808f653
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2024-02-27 00:49:32 -0600

    Funcional

--- clases.h
+++ clases.h
@@ -13,11 +13,19 @@ using namespace std;
 
 extern int totalCuentas;
 
-void continuar(){
-    cout<<"Presione entrar para continuar . . ."<<endl;
-    cin.get();
-    system(CLEAR);
-}
+const int INICIAR_SESION = 1;
+const int CREAR_CUENTA_DEBITO = 2;
+const int CREAR_CUENTA_CREDITO = 3;
+const int SALIR = 4;
+const int REALIZAR_COMPRA = 5;
+const int REALIZAR_PAGO = 6; 
+const int INFORMACION_CUENTA = 7;
+const int DEPOSITAR = 8; 
+const int RETIRAR = 9;
+const int EXIT = 0;
+
+
+
 
 class Usuario{
     private:
@@ -29,6 +37,10 @@ class Usuario{
             this->nombreCompleto = nombre;
             this->edad = edad;
             this->telefono = telefono;
+        }
+        void continuar(){
+            cout<<"Presione entrar para continuar . . ."<<endl;
+            cin.get();
         }
         void mostrarInformacion(){
             cout<<"Nombre: "<<this->nombreCompleto<<endl;
@@ -48,11 +60,13 @@ class Usuario{
 };
 
 class Cuenta{
+    friend class Banco;
     protected:
         Usuario titular;
         int numeroCuenta = 0;
         int nip;
         float saldo;
+        string concepto;
     public:
         Cuenta(int nip, Usuario titular) : titular(titular) {
             this->saldo = 0;
@@ -60,7 +74,12 @@ class Cuenta{
             this->nip = nip;
             this->titular = titular;
         }
+        void continuar(){
+            cout<<"Presione entrar para continuar . . ."<<endl;
+            cin.get();
+        }
         void mostrarInfo(){
+            system(CLEAR);
             cout<<"---Información del usuario---"<<endl;
             this->titular.mostrarInformacion();
             cout<<"---Información de la cuenta--"<<endl;
@@ -70,23 +89,35 @@ class Cuenta{
         }
 };
 
-class CuentaDebito : protected Cuenta{
+class CuentaDebito : public Cuenta{
     public:
         CuentaDebito(int nip, Usuario titular) : Cuenta(nip, titular) {}
+        void retirarDinero(float monto){
+            system(CLEAR);
+            fflush(stdin);
+            string nombre = this->titular.getNombre();
+            this->saldo -= monto;
+            cout<<"Se han retirado $"<<monto<<" pesos a la cuenta de "<<nombre<<endl;
+            continuar();
+        }
         void depositarDinero(float monto){
+            system(CLEAR);
+            fflush(stdin);
             string nombre = this->titular.getNombre();
             this->saldo += monto;
             cout<<"Se han ingresado $"<<monto<<" pesos a la cuenta de "<<nombre<<endl;
             continuar();
         }
         void obtenerSaldo(){
+            system(CLEAR);
             cout<<"---Monto actual---"<<endl;
             cout<<"Saldo: $"<<this->saldo<<endl;
             cout<<"Numero de cuenta: "<<this->numeroCuenta<<endl;
             cout<<"------------------"<<endl;
-            continuar();
         }
         void informacionCuenta(){
+            system(CLEAR);
+            fflush(stdin);
             const string nombre = this->titular.getNombre();
             const int edad = this->titular.getEdad();
             const string telefono = this->titular.getTelefono();
@@ -100,7 +131,7 @@ class CuentaDebito : protected Cuenta{
         }
 };
 
-class CuentaCredito : protected Cuenta{
+class CuentaCredito : public Cuenta{
     private:
         float deuda = 0;
         float limite;
@@ -115,22 +146,28 @@ class CuentaCredito : protected Cuenta{
             this->fechaLimite = 5;
         }
         void saldoAPagar(){ 
+            system(CLEAR);
             cout<<"---Saldo actual---"<<endl;
             cout<<"Saldo a pagar en la proxima fecha de limite es: "<<this->deuda<<endl;
             cout<<"Limite disponible: "<<limite-deuda<<endl;
-            continuar();
         }
-        void realizarCompra(float monto){
+        void realizarCompra(float monto, string concepto){
+            system(CLEAR);
+            fflush(stdin);
+            this->concepto = concepto;
             this->deuda += monto;
-            cout<<"Compra de $"<<monto<<" pesos realizada correctamente"<<endl;
+            cout<<"Compra de "<<concepto<<" - $"<<monto<<" pesos realizada correctamente"<<endl;
             continuar();
         }
         void realizarPago(float monto){
+            system(CLEAR);
             this->deuda -= monto;
             cout<<"Pago de $"<<monto<<" pesos realizado correctamente"<<endl;
             continuar();
         }
         void informacionCuenta(){
+            system(CLEAR);
+            fflush(stdin);
             const string nombre = this->titular.getNombre();
             const int edad = this->titular.getEdad();
             const string telefono = this->titular.getTelefono();
@@ -143,22 +180,51 @@ class CuentaCredito : protected Cuenta{
             continuar();
         }
 };
-/**---menu principal---
- * 1-iniciar sesion
- * 2-crear cuenta de credito 
- * 3-crear cuenta de debito
- * 4-salir
-*/
-/**---submenu de la opcion 1---
- * 1
-*/
 
 class Banco{
     private:
-        vector<CuentaDebito*> cuentasDebito;
-        vector<CuentaCredito*> cuentasCredito;
+        vector<CuentaDebito> cuentasDebito;
+        vector<CuentaCredito> cuentasCredito;
+        int opc;
     public:
-        Usuario* crearUsuario(){
+        void continuar(){
+            cout<<"Presione entrar para continuar . . ."<<endl;
+            cin.get();
+        }
+        void menuPrincipal(){
+            system(CLEAR);
+            cout<<"---Menu principal---"<<endl;
+            cout<<"1-Iniciar sesion"<<endl;
+            cout<<"2-Crear cuenta de debito"<<endl;
+            cout<<"3-Crear cuenta de credito"<<endl;
+            cout<<"0-EXIT"<<endl;
+            cout<<"Opcion: ";
+            cin>>this->opc;
+            fflush(stdin);
+            system(CLEAR);
+        }
+        void subMenuCredito(){
+            cout<<"---Cuenta de credito---"<<endl;
+            cout<<"5-Realizar compra"<<endl;
+            cout<<"6-Realizar pago"<<endl;
+            cout<<"7-Informacion de la cuenta"<<endl;
+            cout<<"4-Salir de la cuenta"<<endl;
+            cout<<"Opcion: ";
+            cin>>this->opc;
+            fflush(stdin);
+        }
+        void subMenuDebito(){
+            cout<<"---Cuenta de debito---"<<endl;
+            cout<<"8-Depositar"<<endl;
+            cout<<"9-Retirar"<<endl;
+            cout<<"7-Informacion de la cuenta"<<endl;
+            cout<<"4-Salir de la cuenta"<<endl;
+            cout<<"Opcion: ";
+            cin>>this->opc;
+            fflush(stdin);
+        }
+        Usuario crearUsuario(){
+            system(CLEAR);
             string nombre; 
             int edad; 
             string telefono;
@@ -172,56 +238,167 @@ class Banco{
             cout<<"Ingrese el telefono del titular: ";
             cin>>telefono;
             cin.ignore();
-            Usuario* nuevo = new nuevo(string nombre, int edad, string telefono);
+            Usuario nuevo = Usuario(nombre, edad, telefono);
 
             return nuevo;
         }
         void crearCuentaDebito(){
             int nip;
-            Usuario* nuevo = this->crearUsuario();
+            Usuario nuevo = this->crearUsuario();
             cout<<"---Datos de Cuenta---"<<endl;
             cout<<"Cree un nuevo nip: ";
             cin>>nip;
-            CuentaDebito* nueva = new CuentaDebito(nip, nuevo);
+            fflush(stdin);
+            CuentaDebito nueva = CuentaDebito(nip, nuevo);
             this->cuentasDebito.push_back(nueva);
             continuar();
         }
         void crearCuentaCredito(){
             int nip;
-            Usuario* nuevo = this->crearUsuario();
+            Usuario nuevo = this->crearUsuario();
             cout<<"---Datos de Cuenta---"<<endl;
             cout<<"Cree un nuevo nip: ";
             cin>>nip;
-            CuentaCredito* nueva = new CuentaCredito(nip, nuevo);
-            this->crearCuentaCredito.push_back(nueva); 
+            fflush(stdin);
+            CuentaCredito nueva = CuentaCredito(nip, nuevo);
+            this->cuentasCredito.push_back(nueva); 
             continuar();
         }
-        void iniciarSesion(){
-            bool band = true;
-            string nombre;
-            int nip;
-            cout<<"---Inicio de sesion---"<<endl;
-            cout<<"Usuario: ";
-            cin>>nombre;
-            cin.ignore();
-            cout<<"Nip: ";
-            cin>>nip;
-            cin.ignore();
-            for(int i=0; i<this->cuentasCredito; i++){
-                if(nombre == this->cuentasCredito[i] && nip == this->cuentasCredito[i]){
-                    //submenu
-                }else if(nombre == this->cuentasDebito[i] && nip == this->cuentasDebito[i]){
-                    //submenu
+        void iniciarSesion() {
+    bool band = true;
+    string concepto, nombre;
+    float cantidad;
+    int nip;
+    do {
+        this->menuPrincipal();
+        switch (this->opc) {
+            case INICIAR_SESION: {
+                system(CLEAR);
+                cout << "---Inicio de sesion---" << endl;
+                cout << "Usuario: ";
+                cin >> nombre;
+                cout << "Nip: ";
+                cin >> nip;
+                for (int i = 0; i < this->cuentasCredito.size(); i++) {
+                    if (nombre == this->cuentasCredito[i].titular.getNombre() && nip == this->cuentasCredito[i].nip) {
+                        band = false;
+                        cin.get(); // Espera a que el usuario presione Enter para continuar
+                        do {
+                            this->cuentasCredito[i].saldoAPagar();
+                            this->subMenuCredito();
+                            switch (this->opc) {
+                                case REALIZAR_COMPRA: {
+                                    system(CLEAR);
+                                    cout << "---Compra---" << endl;
+                                    cout << "Ingrese el concepto: ";
+                                    cin >> concepto;
+                                    cout << "Ingrese el monto: ";
+                                    cin >> cantidad;
+                                    this->cuentasCredito[i].realizarCompra(cantidad, concepto);
+                                    break;
+                                }
+                                case REALIZAR_PAGO: {
+                                    system(CLEAR);
+                                    cout << "---Pago---" << endl;
+                                    cout << "Ingrese el monto a pagar: ";
+                                    cin >> cantidad;
+                                    this->cuentasCredito[i].realizarPago(cantidad);
+                                    break;
+                                }
+                                case INFORMACION_CUENTA: {
+                                    system(CLEAR);
+                                    this->cuentasCredito[i].informacionCuenta();
+                                    break;
+                                }
+                                case SALIR: {
+                                    break;
+                                }
+                                default: {
+                                    system(CLEAR);
+                                    cout << "Opcion no valida" << endl;
+                                    continuar();
+                                    break;
+                                }
+                            }
+                        } while (this->opc != SALIR);
+                        break;
+                    }
                 }
+                for (int i = 0; i < this->cuentasDebito.size(); i++) {
+                    if (nombre == this->cuentasDebito[i].titular.getNombre() && nip == this->cuentasDebito[i].nip) {
+                        band = false;
+                        do {
+                            this->cuentasDebito[i].obtenerSaldo();
+                            this->subMenuDebito();
+                            switch (this->opc) {
+                                case DEPOSITAR: {
+                                    system(CLEAR);
+                                    cout << "---Deposito---" << endl;
+                                    cout << "Ingrese el monto: ";
+                                    cin >> cantidad;
+                                    this->cuentasDebito[i].depositarDinero(cantidad);
+                                    fflush(stdin);
+                                    break;
+                                }
+                                case RETIRAR: {
+                                    system(CLEAR);
+                                    cout << "---Retiro---" << endl;
+                                    cout << "Ingrese el monto: ";
+                                    cin >> cantidad;
+                                    this->cuentasDebito[i].retirarDinero(cantidad);
+                                    break;
+                                }
+                                case INFORMACION_CUENTA: {
+                                    system(CLEAR);
+                                    cout << "---Informacion de la cuenta---" << endl;
+                                    this->cuentasDebito[i].informacionCuenta();
+                                    break;
+                                }
+                                case SALIR: {
+                                    break;
+                                }
+                                default: {
+                                    system(CLEAR);
+                                    cout << "Opcion no valida" << endl;
+                                    continuar();
+                                    break;
+                                }
+                            }
+                        } while (this->opc != SALIR);
+                        break;
+                    }
+                }
+                if (band) {
+                    system(CLEAR);
+                    cout << "El usuario ingresado no tiene cuenta actualmente." << endl;
+                    continuar();
+                }
+                break;
             }
-            if(band){
-                cout<<"El usuario ingresado no tiene cuenta actualmete."<<endl;
+            case CREAR_CUENTA_DEBITO: {
+                system(CLEAR);
+                this->crearCuentaDebito();
+                band = false;
+                break;
+            }
+            case CREAR_CUENTA_CREDITO: {
+                system(CLEAR);
+                this->crearCuentaCredito();
+                band = false;
+                break;
+            }
+            case EXIT: {
+                break;
+            }
+            default: {
+                system(CLEAR);
+                cout << "Opcion no valida" << endl;
                 continuar();
+                break;
             }
-
         }
-
-
+    } while (this->opc != EXIT);
+}
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,19 +5,10 @@ using namespace std;
 
 int totalCuentas = 1;
 
-
 int main(){
-    //Creacion de usuario
-    Usuario jorge("Jorge", 21, 3317050177);
-    //Creacion de cuenta de prueba 1
-    CuentaCredito cuenta1(2121, jorge);
-
-    cuenta1.saldoAPagar();
-    cuenta1.realizarCompra(1000);
-    cuenta1.saldoAPagar();
-    cuenta1.realizarPago(500);
-    cuenta1.informacionCuenta();
-
+    
+    Banco bancomi;
+    bancomi.iniciarSesion();
 
     return 0;
 }
